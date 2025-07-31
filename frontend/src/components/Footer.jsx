730fe47fa02964be4656a8efd98934a6679dc685
Charuka Sandaruwan <charukasan156@gmail.com>
2025-07-31 17:39:22 +0530

    Create Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-[#36013F] text-white p-4 text-center">
+      <p>&copy; 2025 Task Manager. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
