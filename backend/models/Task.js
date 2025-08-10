9a74ee5ccd22470ac4d54e0e0f7ee4162e23a813
Charuka Sandaruwan <charukasan156@gmail.com>
2025-08-10 22:31:35 +0530

    update backend code

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -5,6 +5,7 @@ const taskSchema = new mongoose.Schema({
   description: { type: String, required: true },
   dueDate: { type: Date, required: true },
   status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], required: true, default: 'Pending' },
+  priority: { type: String, enum: ['Low', 'Medium', 'High'], required: true, default: 'Medium' }, // New field
 }, { timestamps: true });
 
 export default mongoose.model("Task", taskSchema);
